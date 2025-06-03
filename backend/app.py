67a970dd02e247abd9406836d5e1e97bae82a452
AviAsnani <aviasnani@MACBOOKAIR-3E78.station>
2025-06-03 11:50:36 +0100

    imported models.user -> User class to app.py

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
-from ex import db, bcrypt
+from extensions import db, bcrypt
+from models.user import User
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask_cors import CORS
 
@@ -9,5 +10,11 @@ def create_app():
     app = Flask(__name__)
     db.init_app(app)
     bcrypt.init_app(app)
-    CORS(app)
-    return app  
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = "user_login"
+
+
+    @login_manager.user_loader
+    def load_user(user_id): 
+        return User.query.filter_by(id=int(user_id)).first() 
